27ea24bcf50c63ba0664ee8e8d7e35712266a051
awasaw <75282531+awasaw@users.noreply.github.com>
2021-12-18 19:11:27 +0300

    Update mercury.sh

--- mercury.sh
+++ mercury.sh
@@ -10,6 +10,7 @@ then
     echo "------------------------------"
 fi 
 
+# DATAFILE используется в т.ч. как lock-file
 while [ -f $DATAFILE ]  &&  [ $TRY -ne 0 ]   ; do
     TRY=$(($TRY - 1))
     sleep 1s
@@ -21,14 +22,21 @@ if [ "$TRY" -eq "0" ]
 fi
 
 case $1 in
+# Сюда можно добавлять свои команды
 count) 
-    CMD='\x00\x0B\x63\xC0\x27\xC6\x20' 
+    # комманда сформированная command_gen.py
+    CMD='\x00\x0B\x63\xC0\x27\xC6\x20'
+    
+    # Парсинг ответа счетчика.
+    # Помните, что парсер выполняется командой eval, поэтому необходимо экранировать все спецсимволы.
     PARSER="T1=\${DATA:10:6}.\${DATA:16:2}; \
             T2=\${DATA:18:6}.\${DATA:24:2}; \
             T3=\${DATA:26:6}.\${DATA:32:2}; \
             T4=\${DATA:34:6}.\${DATA:40:2}; \
             printf 'Tarif1: %s  \nTarif2: %s \nTarif3: %s \nTarif4: %s \n' \
             \$T1 \$T2 \$T3 \$T4"
+            
+    # Команда, исполняемая когда указан ключ public
     PUBLIC="/usr/bin/wget -q -O /dev/null http://api.thingspeak.com/update?api_key=ATWRJ7K31\\&field1=\$T1\\&field2=\$T2\\&field3=\$T3\\&field4=\$T4"
     ;;
 *) 
